1aee86f157c27af4367fdb31364b1293bb312035
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-08 12:07:21 +0200

    Zadanie 3 poprawki

--- src/main/java/org/example/zad3/Zad3.java
+++ src/main/java/org/example/zad3/Zad3.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class Zad3 {
     public static void main(String[] args) {
         List<Integer> num = Arrays.asList(1,3,4,10,9,13,6);
-        num.stream().filter(i -> i%2 != 0).forEach(i -> System.out.print(i * 100 + ", "));
+        num.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n*100)
+                .forEach(System.out::println);
     }
 }
